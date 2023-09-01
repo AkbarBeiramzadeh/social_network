b0ae4ff68ddbd0257edbc09392103b9ec15683f4
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-09-01 19:37:57 +0330

    build: build html and css files for posts and members

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class PostsView(View):
+    def get(self, request):
+        return render(request, 'posts/posts.html')
